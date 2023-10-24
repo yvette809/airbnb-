cf22f8d9356679f2af4bc04d0907ccee7a192707
yvette809 <60625477+yvette809@users.noreply.github.com>
2023-10-25 01:50:43 +0200

    fixes

--- app/page.tsx
+++ app/page.tsx
@@ -2,60 +2,68 @@ import Container from "@/app/components/Container";
 import ListingCard from "@/app/components/listings/ListingCard";
 import EmptyState from "@/app/components/EmptyState";
 
-import getListings, { 
-  IListingsParams
-} from "@/app/actions/getListings";
+import getListings, { IListingsParams } from "@/app/actions/getListings";
 import getCurrentUser from "@/app/actions/getCurrentUser";
 import ClientOnly from "./components/ClientOnly";
 
 interface HomeProps {
-  searchParams: IListingsParams
-};
-
-
-const Home = async ({ searchParams }: HomeProps) => {
-
-  if (!searchParams) {
-    return [];
-  }
-  const listings = await getListings(searchParams);
-  const currentUser = await getCurrentUser();
-
-  if (listings.length === 0) {
-    return (
-      <ClientOnly>
-        <EmptyState showReset />
-      </ClientOnly>
-    );
-  }
-
-  return (
-    <ClientOnly>
-      <Container>
-        <div 
-          className="
-            pt-24
-            grid 
-            grid-cols-1 
-            sm:grid-cols-2 
-            md:grid-cols-3 
-            lg:grid-cols-4
-            xl:grid-cols-5
-            2xl:grid-cols-6
-            gap-8
-          "
-        >
-          {listings.map((listing: any) => (
-            <ListingCard
-              currentUser={currentUser}
-              key={listing.id}
-              data={listing}
-            />
-          ))}
-        </div>
-      </Container>
-    </ClientOnly>
-  )
+  searchParams: IListingsParams;
 }
+
+const Home = ({ searchParams }: HomeProps) => {
+  const fetchData = async () => {
+    try {
+      if (!searchParams) {
+        // Handle the case where searchParams is missing or undefined.
+        return [];
+      }
+      
+      const listings = await getListings(searchParams);
+      const currentUser = await getCurrentUser();
+
+      if (listings.length === 0) {
+        return (
+          <ClientOnly>
+            <EmptyState showReset />
+          </ClientOnly>
+        );
+      }
+
+      return (
+        <ClientOnly>
+          <Container>
+            <div
+              className="
+                pt-24
+                grid
+                grid-cols-1
+                sm:grid-cols-2
+                md:grid-cols-3
+                lg:grid-cols-4
+                xl:grid-cols-5
+                2xl:grid-cols-6
+                gap-8
+              "
+            >
+              {listings.map((listing: any) => (
+                <ListingCard
+                  currentUser={currentUser}
+                  key={listing.id}
+                  data={listing}
+                />
+              ))}
+            </div>
+          </Container>
+        </ClientOnly>
+      );
+    } catch (error) {
+      // Handle the error gracefully, e.g., display an error message.
+      console.error("Error fetching data:", error);
+      return <div>Error fetching data.</div>;
+    }
+  };
+
+  return fetchData();
+};
 
 export default Home;
